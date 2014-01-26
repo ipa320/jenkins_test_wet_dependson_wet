67a9c48914bc88c42dd90f43584d2db752c7486b
fmw <fmw@ipa.fhg.de>
2014-01-26 13:38:06 +0100

    wet3 and wet4

---
+++ wet4/test/talker.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+PKG='wet4'
+import roslib; roslib.load_manifest(PKG)
+
+import sys
+import os
+import unittest
+
+class TestSample(unittest.TestCase):
+
+	def test_sample(self):
+		# nothing to report
+		pass
+ 
+if __name__ == '__main__':
+	import rosunit
+	rosunit.unitrun(PKG, 'test_urdf', TestSample) 
